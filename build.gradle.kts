0c50b57bb87d5d0a0deb8e6ad88cd2208593eeee
MTM123 <matiss234@gmail.com>
2022-07-18 00:50:08 +0300

    Fix empty messages on discord message with attachments

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "lv.mtm123"
-version = "1.0-SNAPSHOT"
+version = "1.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/lv/mtm123/quackbridge/discord/GuildListener.kt
+++ src/main/kotlin/lv/mtm123/quackbridge/discord/GuildListener.kt
@@ -3,6 +3,9 @@ package lv.mtm123.quackbridge.discord
 import lv.mtm123.quackbridge.QuackBridge
 import lv.mtm123.quackbridge.commands.CommandManager
 import lv.mtm123.quackbridge.config.Config
+import net.dv8tion.jda.api.entities.EmbedType
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.ReadyEvent
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
@@ -10,23 +13,31 @@ import org.spongepowered.api.Server
 import org.spongepowered.api.scheduler.Task
 import org.spongepowered.api.text.serializer.TextSerializers
 
-class GuildListener(private val plugin: QuackBridge, private val server: Server?, private val config: Config, private val commandManager: CommandManager) : ListenerAdapter() {
+class GuildListener(
+    private val plugin: QuackBridge,
+    private val server: Server?,
+    private val config: Config,
+    private val commandManager: CommandManager
+) : ListenerAdapter() {
 
     override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
         if (event.channel.idLong != this.config.chatChannel
-                || event.isWebhookMessage
-                || event.jda.selfUser.idLong == event.author.idLong) {
+            || event.isWebhookMessage
+            || event.jda.selfUser.idLong == event.author.idLong
+        ) {
             return
         }
 
-        val msg = event.message.contentStripped
+        val msg = createMessage(event.message)
+
         if (commandManager.isPossibleCommand(msg)) {
             event.member?.let { commandManager.handleCommand(it, event.channel, event.message) }
         } else {
             Task.builder().execute(Runnable {
-                val bridgedMsg = this.config.discordChatMessageFormat
-                        .replace("%user%", event.member!!.effectiveName) //Won't be null because we are ignoring webhook messages
-                        .replace("%text%", msg)
+                val bridgedMsg = this.config.discordChatMessageFormat.replace(
+                    "%user%", event.member!!.effectiveName
+                ) //Won't be null because we are ignoring webhook messages
+                    .replace("%text%", msg)
                 server?.broadcastChannel?.send(TextSerializers.FORMATTING_CODE.deserialize(bridgedMsg))
             }).submit(plugin)
         }
@@ -34,6 +45,47 @@ class GuildListener(private val plugin: QuackBridge, private val server: Server?
 
     override fun onReady(event: ReadyEvent) {
         event.jda.getTextChannelById(config.chatChannel)?.sendMessage("** Server started **")?.queue()
+    }
+
+    private fun asUrl(embed: MessageEmbed): String {
+        return when (embed.type) {
+            EmbedType.VIDEO -> {
+                val videoInfo = embed.videoInfo ?: return ""
+                videoInfo.url ?: ""
+            }
+            EmbedType.IMAGE -> {
+                val imageInfo = embed.image ?: return ""
+                imageInfo.url ?: imageInfo.proxyUrl ?: ""
+            }
+            else -> ""
+        }
+    }
+
+    private fun createMessage(message: Message): String {
+        var msg = message.contentStripped
+        if (message.embeds.isNotEmpty()) {
+            message.embeds.forEach {
+                msg = concat(msg, asUrl(it))
+            }
+        }
+
+        if (message.attachments.isNotEmpty()) {
+            message.attachments.forEach {
+                msg = concat(msg, it.url)
+            }
+        }
+
+        return msg
+    }
+
+    private fun concat(msg: String, text: String): String {
+        var newMessage = msg
+        if (msg.isEmpty()) {
+            newMessage += "\n"
+        }
+
+        newMessage += text
+        return newMessage
     }
 
 }
